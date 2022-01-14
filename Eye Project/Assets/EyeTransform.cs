fbb025f0937336100a89f4e833656d211b2a18ee
keli214 <77420536+keli214@users.noreply.github.com>
2022-01-14 14:14:20 -0800

    add output rotation vector

--- Eye Project/Assets/EyeTransform.cs
+++ Eye Project/Assets/EyeTransform.cs
@@ -1,32 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
 public class EyeTransform : MonoBehaviour
 {
     public Transform target;
-    public float movementSpeed = 1f;        //controls moveSpeed of translation
-    public float rotationSpeed = 50f;   //controls rotationSpeed of rotation
+    public float movementSpeed = 1f;   //controls moveSpeed of translation
+    public float rotationSpeed = 2f;   //controls rotationSpeed of rotation
+    
+    //values that you want to save
+    private float x;
+    private float y;
+    private float z;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CreateText();
+    }
+    
+    void write(){
+        string path = Application.dataPath + "/test.txt";
+        StreamWriter writer = new StreamWriter(path,true);
+        writer.WriteLine("x: " + x.ToString() + " y: " + y.ToString() + " z: " + z.ToString());
+        writer.Close();
+    }
+    void CreateText(){
+        string path = Application.dataPath + "/test.txt";
+        if(!File.Exists(path)){
+            File.WriteAllText(path,"Test \n\n");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        x = Random.Range(0,10f);
+        y = Random.Range(20.0f, 20.0f);
+        z = Random.Range(20.0f, 20.0f);
         if (Input.GetKey(KeyCode.UpArrow))
             transform.Translate(transform.forward * movementSpeed * Time.deltaTime);
         if (Input.GetKey(KeyCode.DownArrow))
             transform.Translate(-transform.forward * movementSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.W))
-            transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.S))
-            transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(Vector3.forward * -rotationSpeed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
-            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W)) {
+            transform.Rotate(new Vector3(x, y, z) * rotationSpeed * Time.deltaTime);
+            write();
+        }
+        
     }
 }
