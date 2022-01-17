3e4f6a4ed938631ad12d923378c6b3d364b1743f
jvillazon <jvillazo@ucsd.edu>
2022-01-17 15:06:11 -0800

    Update 1.17.22
    new UI/recording set up

--- Eye Project/Assets/ChangeLightColor.cs
+++ Eye Project/Assets/ChangeLightColor.cs
@@ -35,7 +35,7 @@ public class ChangeLightColor : MonoBehaviour
         rValue = 1f;
         gValue = 1f;
         bValue = 1f;
-        brightness = 0.5f;
+        brightness = 0.75f;
         // newColor = new Color(rValue,gValue,bValue);
         lt.color = new Color(rValue,gValue,bValue);
     }
