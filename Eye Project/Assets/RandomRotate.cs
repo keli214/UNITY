55bdfbd7eca9673bc1ded0ac0f5edc8fae6181ee
keli214 <77420536+keli214@users.noreply.github.com>
2022-01-16 16:01:42 -0800

    random motion update

--- Eye Project/Assets/RandomRotate.cs
+++ Eye Project/Assets/RandomRotate.cs
@@ -5,9 +5,14 @@ using System.IO;
 
 public class RandomRotate : MonoBehaviour
 {
-    private float x;
-    private float y;
-    private float z;
+    private float x = 0;
+    private float y = 0;
+    private float z = 0;
+
+    private bool test = true;
+
+    private int count = 0;
+    private int update = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,10 +30,8 @@ public class RandomRotate : MonoBehaviour
     void CreateText()
     {
         string path = Application.dataPath + "/test.txt";
-        if (!File.Exists(path))
-        {
-            File.WriteAllText(path, "Test \n\n");
-        }
+        File.WriteAllText(path, "Test \n\n");
+        
     }
 
     // Update is called once per frame
@@ -36,13 +39,58 @@ public class RandomRotate : MonoBehaviour
     {
         //if (transform.eulerAngles.x > 180 | transform.eulerAngles.x < 0 |transform.eulerAngles.y > 90 |transform.eulerAngles.y <-90| transform.eulerAngles.z > 90|transform.eulerAngles.z < -90)
         //      transform.rotation = Quaternion.Euler(90, 0, 0);
-        if (Input.GetKey(KeyCode.R)) //press R to rotate from current position 
-        {
-            x = Random.Range(-1f, 1f);
-            y = Random.Range(-1f, 1f);
-            z = Random.Range(-1f, 1f);
-            transform.Rotate(x, y, z);
-            write();
+        // if (Input.GetKey(KeyCode.R)) //press R to rotate from current position 
+        // {
+        count ++;
+        update ++;
+        if(count > 60){
+            count = 0;
         }
+        if(update == 1200){
+            transform.localEulerAngles = new Vector3(90f,0,0);
+            update = 0;
+        }
+        if(count == 20){
+            y = Random.Range(-5.0f, 5.0f);
+            z = Random.Range(-5.0f, 5.0f);
+            if(test){
+                x = Random.Range(9f, 10f);
+                test = false;
+            }
+            else{
+                x = Random.Range(-10f, -9f);
+                test = true;
+            }
+            transform.Rotate(new Vector3(x, y, z));
+        }
+        else if(count == 40){
+            x = Random.Range(-5.0f, 5.0f);
+            z = Random.Range(-5.0f, 5.0f);
+            // y = Random.Range(9f, 10f);
+            if(test){
+                y = Random.Range(9f, 10f);
+                test = false;
+            }
+            else {
+                y = Random.Range(-10f, -9f);
+                test = true;
+            }
+            transform.Rotate(new Vector3(x, y, z));
+        }
+        else if(count == 60){
+            x = Random.Range(-5.0f, 5.0f);
+            y = Random.Range(-5.0f, 5.0f);
+            if(test){
+                z = Random.Range(9f, 10f);
+                test = false;
+            }
+            else {
+                z = Random.Range(-10f, -9f);
+                test = true;
+            }
+            transform.Rotate(new Vector3(x, y, z));
+        }
+        write();
+        
     }
 }
